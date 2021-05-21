5aa88ea4698ef1310960685ca81f4786aaa9d672
meanii <anilchauhanxda@gmail.com>
2021-05-21 11:45:05 +0000

    added LICENSE and added copyright header in every files

--- Stella/plugins/gban/gban_api/promote_apilevel.py
+++ Stella/plugins/gban/gban_api/promote_apilevel.py
@@ -1,3 +1,21 @@
+#    Stella (Development)
+#    Copyright (C) 2021 - meanii (Anil Chauhan)
+#    Copyright (C) 2021 - SpookyGang (Neel Verma, Anil Chauhan)
+
+#    This program is free software; you can redistribute it and/or modify 
+#    it under the terms of the GNU General Public License as published by 
+#    the Free Software Foundation; either version 3 of the License, or 
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from Stella import OWNER_ID, StellaAPI, StellaCli
 from Stella.helper import custom_filter
 
