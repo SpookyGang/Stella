a96df9ac6f3e1cb0295a8a913a0c28af1b26db80
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-08-08 16:07:31 +0530

    Update pyrocovid.py

--- Stella/plugins/covid/pyrocovid.py
+++ Stella/plugins/covid/pyrocovid.py
@@ -1,22 +1,22 @@
-from Stella import pbot as Stella
+from Stella import pbot as Stellacli
 from Stella.pyrogramee.errors import capture_err
 from Stella.pyrogramee.json_prettify import json_prettify
 from Stella.pyrogramee.fetch import fetch
 from pyrogram import filters
 
 
-@Stella.on_message(filters.command("covid") & ~filters.edited)
+@Stellacli.on_message(filters.command("covid") & ~filters.edited)
 @capture_err
 async def covid(_, message):
     if len(message.command) == 1:
         data = await fetch("https://corona.lmao.ninja/v2/all")
         data = await json_prettify(data)
-        await Stella.send_message(message.chat.id, text=data)
+        await Stellacli.send_message(message.chat.id, text=data)
         return
     if len(message.command) != 1:
         country = message.text.split(None, 1)[1].strip()
         country = country.replace(" ", "")
         data = await fetch(f"https://corona.lmao.ninja/v2/countries/{country}")
         data = await json_prettify(data)
-        await Stella.send_message(message.chat.id, text=data)
+        await Stellacli.send_message(message.chat.id, text=data)
         return
