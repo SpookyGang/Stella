e5bf56a8b8a124fda52bcd790c5e38811d1e82b8
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-08-08 16:13:41 +0530

    Create pyrocovid_info.py

---
+++ Stella/plugins/covid/pyrocovid_info.py
@@ -0,0 +1,7 @@
+__mod_name__ = "Covid19"
+
+__help__ = (
+    "Module to get stats of COVID 19 in the world!\n\n"
+    " - /covid : to see global Covid19 stats \n"
+    " - /covid {Country Name} : to see COVID 19 stats for specific country"
+)   
