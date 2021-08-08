4e7f9dadc7fc4b2f42fa76ac38dc7000fcdae401
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-08-08 16:02:51 +0530

    Create pyrocovid.py

---
+++ Stella/plugins/covid/pyrocovid.py
@@ -0,0 +1,22 @@
+from EmceeBot import pbot as app
+from EmceeBot.pyrogramee.errors import capture_err
+from EmceeBot.pyrogramee.json_prettify import json_prettify
+from EmceeBot.pyrogramee.fetch import fetch
+from pyrogram import filters
+
+
+@app.on_message(filters.command("covid") & ~filters.edited)
+@capture_err
+async def covid(_, message):
+    if len(message.command) == 1:
+        data = await fetch("https://corona.lmao.ninja/v2/all")
+        data = await json_prettify(data)
+        await app.send_message(message.chat.id, text=data)
+        return
+    if len(message.command) != 1:
+        country = message.text.split(None, 1)[1].strip()
+        country = country.replace(" ", "")
+        data = await fetch(f"https://corona.lmao.ninja/v2/countries/{country}")
+        data = await json_prettify(data)
+        await app.send_message(message.chat.id, text=data)
+        return
